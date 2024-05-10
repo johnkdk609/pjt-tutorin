0d02c4a89b678fc29fae2b0850885d25dab15388
Dongkyu <johnkdk609@gmail.com>
2024-05-10 17:17:33 +0900

    Initial commit to set up the TutorIn project structure and configurations.

---
+++ tutorin/src/main/java/com/pjt/tutorin/TutorinApplication.java
@@ -0,0 +1,13 @@
+package com.pjt.tutorin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorinApplication.class, args);
+	}
+
+}
---
+++ tutorin/src/test/java/com/pjt/tutorin/TutorinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjt.tutorin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
