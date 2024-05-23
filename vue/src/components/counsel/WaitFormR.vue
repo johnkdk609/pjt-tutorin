2cecd76fca0bf6739daa400417a9e9704b294976
mincheol <mckim19980223@gmail.com>
2024-05-24 04:22:33 +0900

    Feat: final request

--- vue/src/components/counsel/WaitFormR.vue
+++ vue/src/components/counsel/WaitFormR.vue
@@ -1,33 +1,36 @@
 <template>
-  <div>
-    <h4>추가질문</h4>
-    <h5>상담 제목: {{ store.counsel.title }}</h5>
-    <div class="center">
-      <div class="chatbox">
-        <div v-if="store.counsel.question1 || store.counsel.answer1" class="chat">
-          <p v-if="store.counsel.question1" class="question">{{ store.counsel.question1 }}</p>
-          <p v-if="store.counsel.answer1" class="answer">{{ store.counsel.answer1 }}</p>
-        </div>
-        <div v-if="store.counsel.question2 || store.counsel.answer2" class="chat">
-          <p v-if="store.counsel.question2" class="question">{{ store.counsel.question2 }}</p>
-          <p v-if="store.counsel.answer2" class="answer">{{ store.counsel.answer2 }}</p>
-        </div>
-        <div v-if="store.counsel.question3 || store.counsel.answer3" class="chat">
-          <p v-if="store.counsel.question3" class="question">{{ store.counsel.question3 }}</p>
-          <p v-if="store.counsel.answer3" class="answer">{{ store.counsel.answer3 }}</p>
-        </div>
-        <div v-if="store.counsel.status === 3" class="input-group mt-3">
-          <input type="text" placeholder="질문을 등록해주세요" v-model="input" class="form-control" />
-          <div class="input-group-append">
-            <button class="btn btn-primary" @click="submitQuestion">질문 등록</button>
-          </div>
-        </div>
+  <div class="container mt-4">
+  <div class="card">
+    <div class="card-header">
+      상담 상태 : {{ store.counselStatus[store.counsel.status - 1] }}
+    </div>
+    <div class="card-body">
+      <table class="table table-bordered table-custom">
+        <tr>
+          <td class="bg-gray">제목</td>
+          <td colspan="3">{{ store.counsel.title }}</td>
+        </tr>
+        <tr>
+          <td class="bg-gray">멘티(작성자)</td>
+          <td>{{ store.counsel.menteeId }}</td>
+          <td class="bg-gray">멘토</td>
+          <td>{{ store.counsel.mentorId }}</td>
+        </tr>
+        <tr style="height: 200px;">
+          <td class="bg-gray">질문 내용</td>
+          <td colspan="3">{{ store.counsel.content }}</td>
+        </tr>
+      </table>
+      <div class="text-right">
+        <button class="btn btn-success" @click="accept">수락</button>
+        <button class="btn btn-danger" @click="reject">거절</button>
       </div>
     </div>
-    <div class="text-right mt-2" style="color: rgb(212, 212, 212);">
-      counsel id : {{ store.counsel.id }}
-    </div>
   </div>
+  <div class="text-right mt-2" style="color: rgb(212, 212, 212);">
+    counsel id : {{ store.counsel.id }}
+  </div>
+</div>
 </template>
 
 <script setup>
@@ -59,6 +62,7 @@ const reject = () => {
 </script>
 
 <style scoped>
+
 .bg-gray {
   background-color: rgb(206, 206, 206);
   color: rgb(80, 80, 80);
